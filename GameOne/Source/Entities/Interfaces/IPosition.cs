6afabdc865d2e0b4c44df772b405f14aa5eacd91
Galya Georgieva <alice.gospodinova@gmail.com>
2016-06-04 14:01:57 +0300

    Change file structure accordingly diagram. Made some changes in Entity classes.

--- GameOne/Source/Entities/Interfaces/IPosition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameOne.Source.Entities.Interfaces
-{
-    using Microsoft.Xna.Framework;
-
-    public interface IPosition
-    {
-        Vector2 Position { get; set; }
-    }
-}
--- GameOne/Source/Renderer/Spritesheet.cs
+++ GameOne/Source/Renderer/Spritesheet.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameOne.Source.Renderer
 {
-	class Spritesheet
+    public class Spritesheet
 	{
 		// Methods and information for loading and sequencing series of frames into animations
 		// Individual frames and sequences may be separate classes
