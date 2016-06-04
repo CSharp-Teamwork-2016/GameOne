24a9500e4063ff9be36e81993277f06281e3201f
Galya Georgieva <alice.gospodinova@gmail.com>
2016-06-04 20:53:28 +0300

    Some base code for Level and Tile classes

--- GameOne/Source/Level/Tile.cs
+++ GameOne/Source/Level/Tile.cs
@@ -1,10 +1,35 @@
 ﻿namespace GameOne.Source.Level
 {
-	class Tile
+    using GameOne.Source.Enumerations;
+
+    public class Tile
 	{
-		// 1x1 square from the playable level
-		// Can be either transparent (floor, no collisions), opaque (walls) or clip (mobs collide, projectiles and effects pass trough)
-		// Must implement IRenderable, for z-level sorting by the same method as Entities
-		// Chlid classes may include active level elements, like doors, buttons or traps
-	}
+        // 1x1 square from the playable level
+        // Can be either transparent (floor, no collisions), opaque (walls) or clip (mobs collide, projectiles and effects pass trough)
+        // Must implement IRenderable, for z-level sorting by the same method as Entities
+        // Chlid classes may include active level elements, like doors, buttons or traps
+
+        private int x, y;
+
+        private TileType tileType;
+
+        private bool transparent = false;
+
+        public Tile(int x, int y, TileType tileType)
+        {
+            this.x = x;
+            this.y = y;
+            this.tileType = tileType;
+        }
+        
+        public void SetTileType(TileType tileType)
+        {
+            this.tileType = tileType;
+        }
+
+        public void SetTransparancy()
+        {
+            this.transparent = true;
+        }
+    }
 }
