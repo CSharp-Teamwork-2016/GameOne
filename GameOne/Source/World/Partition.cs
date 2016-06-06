e1f11d4a1a47cbe95ecb5ed8e8d2bea497e5cdbc
Yavor Milinov <iavor.milinov@gmail.com>
2016-06-06 23:09:22 +0300

    Hallway, Partition, Room in World folder added. They are not finished.

---
+++ GameOne/Source/World/Partition.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOne.Source.World
+{
+    public class Partition
+    {
+        private Partition parent;
+        private Partition leftLeaf;
+        private Partition rightLeaf;
+
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        private bool direction; // false = horizontal split; true = vertical split
+
+        private Room room;
+        private Hallway hallway;
+
+        public Partition(int x, int y, int width, int height, Partition parent)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.parent = parent;
+        }
+
+        //=====================================================
+        //region Properties
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+            }
+        }
+
+        //not sure
+        public bool IsHorizontal
+        {
+            get
+            {
+                return direction;
+            }
+            set
+            {
+            }
+        }
+
+        public Partition LeftLeaf
+        {
+            get
+            {
+                return leftLeaf;
+            }
+            set
+            {
+            }
+        }
+
+        public Partition RightLeaf
+        {
+            get
+            {
+                return rightLeaf;
+            }
+            set
+            {
+            }
+        }
+
+        public Room Room
+        {
+            get
+            {
+                return room;
+            }
+            set
+            {
+            }
+        }
+
+        public Hallway Hallway
+        {
+            get
+            {
+                return hallway;
+            }
+            set
+            {
+            }
+        }
+
+        //not sure
+        public bool HasLeaves
+        {
+            get
+            {
+                return leftLeaf != null;
+            }
+            set
+            {
+            }
+        }
+
+        //end region ============================
+
+        //========================================================
+
+        //region Generation
+
+        public bool trySplit()
+        {
+            //not sure
+            if (HasLeaves)
+            {
+                return leftLeaf.trySplit() || rightLeaf.trySplit();
+            }
+            else
+            {
+                return split();
+            }
+        }
+
+        //TODO
+        //private bool split()
+        //{
+        //    direction = false; // false = horizontal, true = vertical
+        //    if (width / height > LevelMaker.Max)
+        //    {
+
+        //    }
+
+
+        //}
+
+
+
+    }
+}
---
+++ GameOne/Source/World/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOne.Source.World
+{
+    class Room
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        private int originX;
+        private int originY;
+
+        public Room(int x, int y, int width, int height)
+        {
+        }
+    }
+}
