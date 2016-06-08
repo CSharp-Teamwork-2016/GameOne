5f0a4ceee83ca49dad98e60f3f96c702435ca4d2
viktorpts <viktor.pts@abv.bg>
2016-06-08 22:52:06 +0300

    We actually need this static field, or else all entiteis have the same ID

--- GameOne/Source/Entities/Entity.cs
+++ GameOne/Source/Entities/Entity.cs
@@ -3,14 +3,15 @@
     // Base class for all non-geometry (non-Tile, see Level) objects
     public abstract class Entity
     {
+        private static ulong nextId = 0;
         private ulong id;
 
         protected Entity()
         {
-            this.id = 0;
+            this.id = nextId++;
         }
 
-        public ulong Id => this.id++;
+        public ulong Id => this.id;
 
         public abstract void Update(double time);
     }
